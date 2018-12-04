db5d3de6b587173bcdbc0acc72903f6201e4c291
Daniel Keane <keane.dp@gmail.com>
2018-12-04 15:20:51 -0600

    add key bindings for font sizes and window navigation

--- customizations/keybindings.el
+++ customizations/keybindings.el
@@ -1,3 +1,10 @@
 (global-set-key (kbd "C-x C-b") 'ibuffer)
 (global-set-key (kbd "C-x K") 'kill-buffer-and-window)
 
+(global-set-key (kbd "C-+") 'text-scale-increase)
+(global-set-key (kbd "C--") 'text-scale-decrease)
+
+;; Window switching. (C-x o goes to the next window)
+(global-set-key (kbd "C-x O") (lambda ()
+                                (interactive)
+                                (other-window -1))) ;; back one
