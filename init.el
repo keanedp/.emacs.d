9c285add24bc2e6ae29dc00ad2b21d421c12c4bd
Daniel Keane <keane.dp@gmail.com>
2018-12-05 15:07:20 -0600

    switch from parinfer to smartparens and add projectile-ripgrep to C-p

--- init.el
+++ init.el
@@ -11,6 +11,8 @@
 (add-hook 'minibuffer-setup-hook #'my-minibuffer-setup-hook)
 (add-hook 'minibuffer-exit-hook #'my-minibuffer-exit-hook)
 
+(setq-default indent-tabs-mode nil)
+
 (require 'package)
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives
@@ -35,12 +37,13 @@
 ;;   :defer t
 ;;   :init
 ;;   (when (memq window-system '(mac ns))
-;;     (exec-path-from-shell-initialize)))
+;;     (exec-path-from-shell-initialize)
+;;     (exec-path-from-shell-copy-env "PATH")))
 
-(use-package evil
-  :ensure t
-  :init
-  (evil-mode 1))
+;; (use-package evil
+;;   :ensure t
+;;   :init
+;;   (evil-mode 1))
 
 (use-package ido-completing-read+
   :ensure t
@@ -53,11 +56,16 @@
   (ido-ubiquitous-mode t)
   (ido-everywhere t))
 
+(use-package ripgrep
+  :ensure t
+  :defer t)
+
 (use-package projectile
   :ensure t
   :config
   (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
   (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+  (define-key projectile-mode-map (kbd "C-p") 'projectile-ripgrep)
   :init
   (projectile-mode))
 
@@ -68,10 +76,6 @@
   (recentf-mode 1)
   (setq recentf-max-menu-items 40)
   (global-set-key "\C-x\ \C-r" 'recentf-open-files))
-
-(use-package rainbow-delimiters
-  :ensure t
-  :defer t)
 
 (use-package clojure-mode-extra-font-locking
   :ensure t
@@ -113,9 +117,6 @@
   (add-to-list 'auto-mode-alist '("\\.cljs.*$" . clojure-mode))
   (add-to-list 'auto-mode-alist '("lein-env" . enh-ruby-mode)))
 
-;; (use-package lispy
-;;   :ensure t)
-
 (use-package company
   :ensure t
   :init
@@ -149,36 +150,92 @@
   (add-hook 'web-mode-hook  'my-web-mode-hook)
   (add-hook 'web-mode-hook  'emmet-mode) )
 
-(use-package parinfer
+(use-package smartparens
   :ensure t
-  :defer t
-  :bind
-  (:map parinfer-mode-map
-        ("<tab>" . parinfer-smart-tab:dwim-right)
-        ("C-i" . parinfer--reindent-sexp)
-        ("C-M-i" . parinfer-auto-fix)
-        ("C-," . parinfer-toggle-mode)
-        :map parinfer-region-mode-map
-        ("C-i" . indent-for-tab-command)
-        ("<tab>" . parinfer-smart-tab:dwim-right))
-  :config
-  (setq parinfer-auto-switch-indent-mode nil)
-  :init
-  (progn
-    (setq parinfer-extensions
-      '(defaults       ; should be included.
-         pretty-parens  ; different paren styles for different modes.
-         evil           ; If you use Evil.
-         ;;lispy          ; If you use Lispy. With this extension, you should install Lispy and do not enable lispy-mode directly.
-         ;;paredit        ; Introduce some paredit commands.
-         smart-tab      ; C-b & C-f jump positions and smart shift with tab & S-tab.
-         smart-yank))   ; Yank behavior depend on mode.
-    (add-hook 'clojure-mode-hook #'parinfer-mode)
-    (add-hook 'emacs-lisp-mode-hook #'parinfer-mode)
-    (add-hook 'common-lisp-mode-hook #'parinfer-mode)
-    (add-hook 'scheme-mode-hook #'parinfer-mode)
-    ;;(add-hook 'cider-repl-mode-hook #'parinfer-mode)
-    (add-hook 'lisp-mode-hook #'parinfer-mode)))
+  :config (progn
+            (require 'smartparens-config)
+            (smartparens-global-mode t)
+            ;; highlight matching pairs
+            (show-smartparens-global-mode t)
+            (add-hook 'clojure-mode-hook 'smartparens-strict-mode)
+            (add-hook 'emacs-lisp-mode-hook 'smartparens-strict-mode))
+            (define-key smartparens-mode-map (kbd "C-M-f") 'sp-forward-sexp)
+            (define-key smartparens-mode-map (kbd "C-M-b") 'sp-backward-sexp)
+
+            (define-key smartparens-mode-map (kbd "C-M-d") 'sp-down-sexp)
+            (define-key smartparens-mode-map (kbd "C-M-a") 'sp-backward-down-sexp)
+            (define-key smartparens-mode-map (kbd "C-S-d") 'sp-beginning-of-sexp)
+            (define-key smartparens-mode-map (kbd "C-S-a") 'sp-end-of-sexp)
+
+            (define-key smartparens-mode-map (kbd "C-M-e") 'sp-up-sexp)
+            (define-key smartparens-mode-map (kbd "C-M-u") 'sp-backward-up-sexp)
+            (define-key smartparens-mode-map (kbd "C-M-t") 'sp-transpose-sexp)
+
+            (define-key smartparens-mode-map (kbd "C-M-n") 'sp-forward-hybrid-sexp)
+            (define-key smartparens-mode-map (kbd "C-M-p") 'sp-backward-hybrid-sexp)
+
+            (define-key smartparens-mode-map (kbd "C-M-k") 'sp-kill-sexp)
+            (define-key smartparens-mode-map (kbd "C-M-w") 'sp-copy-sexp)
+
+            (define-key smartparens-mode-map (kbd "M-<delete>") 'sp-unwrap-sexp)
+            (define-key smartparens-mode-map (kbd "M-<backspace>") 'sp-backward-unwrap-sexp)
+
+            (define-key smartparens-mode-map (kbd "C-<right>") 'sp-forward-slurp-sexp)
+            (define-key smartparens-mode-map (kbd "C-<left>") 'sp-forward-barf-sexp)
+            (define-key smartparens-mode-map (kbd "C-M-<left>") 'sp-backward-slurp-sexp)
+            (define-key smartparens-mode-map (kbd "C-M-<right>") 'sp-backward-barf-sexp)
+
+            (define-key smartparens-mode-map (kbd "M-D") 'sp-splice-sexp)
+            (define-key smartparens-mode-map (kbd "C-M-<delete>") 'sp-splice-sexp-killing-forward)
+            (define-key smartparens-mode-map (kbd "C-M-<backspace>") 'sp-splice-sexp-killing-backward)
+            (define-key smartparens-mode-map (kbd "C-S-<backspace>") 'sp-splice-sexp-killing-around)
+
+            (define-key smartparens-mode-map (kbd "C-]") 'sp-select-next-thing-exchange)
+            (define-key smartparens-mode-map (kbd "C-<left_bracket>") 'sp-select-previous-thing)
+            (define-key smartparens-mode-map (kbd "C-M-]") 'sp-select-next-thing)
+
+            (define-key smartparens-mode-map (kbd "M-F") 'sp-forward-symbol)
+            (define-key smartparens-mode-map (kbd "M-B") 'sp-backward-symbol)
+
+            (define-key smartparens-mode-map (kbd "C-\"") 'sp-change-inner)
+            (define-key smartparens-mode-map (kbd "M-i") 'sp-change-enclosing))
+
+;; (use-package rainbow-delimiters
+;;   :ensure t
+;;   :defer t)
+
+;; (use-package parinfer
+;;   :ensure t
+;;   :defer t
+;;   :bind
+;;   (:map parinfer-mode-map
+;;         ("<tab>" . parinfer-smart-tab:dwim-right)
+;;         ("S-<tab>" . parinfer-smart-tab:dwim-left)
+;;         ("C-i" . parinfer--reindent-sexp)
+;;         ("C-M-i" . parinfer-auto-fix)
+;;         ("C-," . parinfer-toggle-mode)
+;;         :map parinfer-region-mode-map
+;;         ("C-i" . indent-for-tab-command)
+;;         ("<tab>" . parinfer-smart-tab:dwim-right)
+;;         ("S-<tab>" . parinfer-smart-tab:dwim-left))
+;;   :config
+;;   (setq parinfer-auto-switch-indent-mode nil)
+;;   :init
+;;   (progn
+;;     (setq parinfer-extensions
+;;       '(defaults       ; should be included.
+;;          pretty-parens  ; different paren styles for different modes.
+;;          ;;evil           ; If you use Evil.
+;;          ;;lispy          ; If you use Lispy. With this extension, you should install Lispy and do not enable lispy-mode directly.
+;;          ;;paredit        ; Introduce some paredit commands.
+;;          smart-tab      ; C-b & C-f jump positions and smart shift with tab & S-tab.
+;;          smart-yank))   ; Yank behavior depend on mode.
+;;     (add-hook 'clojure-mode-hook #'parinfer-mode)
+;;     (add-hook 'emacs-lisp-mode-hook #'parinfer-mode)
+;;     (add-hook 'common-lisp-mode-hook #'parinfer-mode)
+;;     (add-hook 'scheme-mode-hook #'parinfer-mode)
+;;     ;;(add-hook 'cider-repl-mode-hook #'parinfer-mode)
+;;     (add-hook 'lisp-mode-hook #'parinfer-mode)))
 
 (use-package which-key
   :ensure t
